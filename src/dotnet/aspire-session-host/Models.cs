de6639cd99fbaf24bc9af95ffbbd281643c81be5
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2024-03-08 19:13:50 +0100

    Support new session api

--- src/dotnet/aspire-session-host/Models.cs
+++ src/dotnet/aspire-session-host/Models.cs
@@ -4,11 +4,35 @@ namespace AspireSessionHost;
 
 [UsedImplicitly]
 internal sealed record Session(
-    string ProjectPath,
-    bool Debug,
+    LaunchConfiguration[] LaunchConfigurations,
     EnvironmentVariable[]? Env,
     string[]? Args
 );
 
 [UsedImplicitly]
+internal sealed record LaunchConfiguration(
+    string Type,
+    string ProjectPath,
+    Mode? Mode,
+    string[]? LaunchProfile,
+    bool? DisableLaunchProfile
+);
+
+internal enum Mode
+{
+    Debug,
+    NoDebug
+}
+
+[UsedImplicitly]
 internal sealed record EnvironmentVariable(string Name, string? Value);
+
+[UsedImplicitly]
+internal sealed record ErrorResponse(ErrorDetail Error);
+
+[UsedImplicitly]
+internal sealed record ErrorDetail(
+    string Code,
+    string Message,
+    ErrorDetail[]? Details = null
+);
